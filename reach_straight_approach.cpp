b99834b7e16bb2507b4caf30ca8894dae4b1dbd3
Ubuntu <ubuntu@ip-10-99-0-70.ap-northeast-2.compute.internal>
2026-07-23 22:54:39 +0000

    chore: seed straight approach module

---
+++ reach_straight_approach.cpp
@@ -0,0 +1,4 @@
+#include "reach_straight_approach.h"
+
+// Straight-in grasp approach behavior is implemented test-first by the
+// supervised feature job.
---
+++ reach_straight_approach.h
@@ -0,0 +1,4 @@
+#pragma once
+
+// Straight-in grasp approach interfaces are implemented test-first by the
+// supervised feature job.
